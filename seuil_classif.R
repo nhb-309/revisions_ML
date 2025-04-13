5fcb8948ca2c90f36079f873925dc265a88d351f
nhb-309 <nhbosc@gmail.com>
2025-04-13 16:41:33 +0200

    ok

--- seuil_classif.R
+++ seuil_classif.R
@@ -1,6 +1,9 @@
 library(glmnet)
 library(bestglm)
 library(ranger)
+library(pROC)
+library(ggplot2)
+library(tidyverse)
 
 data(SAheart)
 
@@ -14,6 +17,10 @@ SCORE
 
 nbbloc=5
 bloc = rep(0, nrow(maladie))
+
+maladieX = model.matrix(chd~. , data=maladie)[,-1]
+maladieY = maladie[,"chd"]
+
 ind0=which(maladie$chd == 0)
 ind1=which(maladie$chd == 1)
 
@@ -21,7 +28,7 @@ bloc[ind0] = sample(rep(1:nbbloc,length = length(ind0)))
 bloc[ind1] = sample(rep(1:nbbloc,length = length(ind1)))
 
 models = list()
-i=2
+
 for(i in 1:nbbloc ){
     print(i)
     maladieA = maladie[bloc!=i,]
@@ -36,69 +43,84 @@ for(i in 1:nbbloc ){
     
     choix = bestglm(maladieA, family=binomial)
     SCORE[bloc == i, "choix"] = predict(choix$BestModel, maladieT, type='response')
+
+    maladieXA = maladieX[bloc!=i,]
+    maladieYA = maladieY[bloc!=i]
+    maladieXT = maladieX[bloc==i,]
+
+    #ridge
+    tmp = cv.glmnet(maladieXA, maladieYA, alpha=0, family='binomial')
+    SCORE[bloc==i,'ridge'] = predict(tmp,maladieXT,"lambda.1se",type='response')
     
-    tmp = choix$BestModels[1,-ncol(maladie)]
-    var = names(tmp)[tmp==TRUE]
-    models[[i]]=var
+    #lasso
+    tmp = cv.glmnet(maladieXA, maladieYA, alpha=1, family='binomial')
+    SCORE[bloc==i,'lasso'] = predict(tmp,maladieXT,"lambda.1se",type='response')
+    
+    #elnet
+    tmp = cv.glmnet(maladieXA, maladieYA, alpha=0.5, family='binomial')
+    SCORE[bloc==i,'elnet'] = predict(tmp,maladieXT,"lambda.1se",type='response')
+    
     
     # logistique 
-    mod2 <- step(reglog,trace=0)
-    SCORE[bloc==i,"AIC"] <- predict(mod2,maladieT,type="response")
-    mod3 <- step(reglog,trace=0,k=log(nrow(maladieA)))
-    SCORE[bloc==i,"BIC"] <- predict(mod3,maladieT,type="response")
+    #mod2 <- step(reglog,trace=0)
+    #SCORE[bloc==i,"AIC"] <- predict(mod2,maladieT,type="response")
+    #mod3 <- step(reglog,trace=0,k=log(nrow(maladieA)))
+    #SCORE[bloc==i,"BIC"] <- predict(mod3,maladieT,type="response")
     
-    ### foret
-    hpForet = expand.grid(num.trees = c(100,400,600,800),
-                          max.depth = c(1,3,5),
-                          min.bucket = c(1,2,3),
-                          mtry=c(1,2,3,4,5)
-                          )
-    hpForetParams = data.frame(hpForet,'auc'=NA);hpForetParams
-    for(xx in 1:nrow(hpForet)){
-        foret <- ranger(factor(chd)~.,data=maladieA,
-                        classification = T,
-                        probability = T,
-                        num.trees=hpForet$num.trees[xx],
-                        max.depth =hpForet$max.depth[xx])
-        hpForetResult = predict(foret,maladieT,type='response')
-        hpForetParams[xx,'auc']=roc(maladieT$chd,hpForetResult$predictions[,2])$auc
-        
-    }
-    bestForet=hpForetParams[which.max(hpForetParams$auc),]
-    bestFitForet=ranger(factor(chd)~.,data=maladieA,
-                   classification = T, probability = T, 
-                   num.trees=bestForet$num.trees,
-                   max.depth = bestForet$max.depth,
-                   min.bucket = bestForet$min.bucket,
-                   mtry = bestForet$mtry)
-    SCORE[bloc==i,"foret"] <- predict(foret,maladieT,type="response")$predictions[,2]
+    #### foret
+    #
+    #
+    #hpForet = expand.grid(num.trees = c(100,400,600,800),
+    #                      max.depth = c(1,3,5),
+    #                      min.bucket = c(1,2,3),
+    #                      mtry=c(1,2,3,4,5)
+    #                      )
+    #hpForetParams = data.frame(hpForet,'auc'=NA);hpForetParams
+    #
+    #for(xx in 1:nrow(hpForet)){
+    #    foret <- ranger(factor(chd)~.,data=maladieA,
+    #                    classification = T,
+    #                    probability = T,
+    #                    num.trees=hpForet$num.trees[xx],
+    #                    max.depth =hpForet$max.depth[xx])
+    #    hpForetResult = predict(foret,maladieT,type='response')
+    #    hpForetParams[xx,'auc']=roc(maladieT$chd,hpForetResult$predictions[,2])$auc
+    #    
+    #}
+    #
+    #bestForet=hpForetParams[which.max(hpForetParams$auc),] # sélection du meilleur hyper paramétrage
+    #
+    #bestFitForet=ranger(factor(chd)~.,data=maladieA,
+    #               classification = T, probability = T, 
+    #               num.trees=bestForet$num.trees,
+    #               max.depth = bestForet$max.depth,
+    #               min.bucket = bestForet$min.bucket,
+    #               mtry = bestForet$mtry)
+    #SCORE[bloc==i,"foret"] <- predict(bestFitForet,maladieT,type="response")$predictions[,2]
     
     
 }
 
 SCORE
 
-###### Résultats hyperparamétrage ----
-## 1. Exploration de l'hyperparamétrage Foret ----
-ggplot(hpForetParams)+
-    aes(x=num.trees,y=auc,group=num.trees,col=as.factor(max.depth))+
-    geom_jitter(alpha=0.7)+theme_light()
-
-
-
-SCORE
-
-
-library(pROC)
 
 rocCV= roc(Y~., data=SCORE)
-SCORE
-aucmodele=sort(round(unlist(lapply(rocCV,auc)),3),dec=TRUE)
 
-lapply(rocCV,FUN=auc) %>% unlist()
-#auc(rocCV$glm) 
-#auc(rocCV$choix)
-#auc(rocCV$foret)
+aucmodele=sort(round(unlist(lapply(rocCV,auc)),5),dec=TRUE)
+aucmodele
+lapply(rocCV,FUN=auc) %>% unlist() %>% round(4) 
+
+plot(rocCV$foret,col='blue')
+lines(rocCV$glm,col='red')
+legend("bottomright",legend=c('foret','log'),col=c('blue','red'))
+
+ind=order(aucmodele,decreasing=T); L = length(ind)
+mapply(plot,rocCV,col=1:L,add=T)
+
+auc(rocCV$glm) 
+auc(rocCV$choix)
+auc(rocCV$foret)
+
 
 
 ind=order(aucmodele,decreasing=T); L = length(ind)
@@ -110,6 +132,6 @@ legend("bottomright",legend = names(SCORE)[2:L], col=2:L, lty=2:L, lwd=1,cex=1)
 
 
 tmp = lapply(rocCV, FUN = coords, x='best', ret = c('threshold','tp','fp','tn','fn','sensitivity','specificity','ac'),transpose = T)
-
+tmp
 mat=do.call(rbind, tmp)
 mat
