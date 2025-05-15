b23a6220277fc6888cffe46832100568945e2e7a
Aetsibb <57639737+nhb-309@users.noreply.github.com>
2025-05-15 23:01:18 +0200

    Update and rename FINAL_CLASSIF_CORRECTION to FINAL_CLASSIF_CORRECTION.R

--- FINAL_CLASSIF_CORRECTION.R
+++ FINAL_CLASSIF_CORRECTION.R
@@ -10,6 +10,7 @@ install.packages('recipes')
 install.packages('bestglm')
 install.packages('kernlab')
 
+library(bestglm)
 library(caret) # necessaire pour la validation croisee stratifiee et le cv
 library(tidyverse)
 library(pROC)
@@ -22,10 +23,14 @@ library(recipes)
 library(bestglm)
 library(kernlab)
 
-# Lecture des données -----
+ncores = parallel::detectCores()
 
 # Répertoire de travail
 setwd("F:/DataScientist/revisions_ML/")
+
+db = SAheart %>% 
+    rename(Y=chd) %>% 
+    mutate(Y=as.factor(Y))
 
 # lecture
 db <- read.csv('bank.csv',sep=';')  %>% 
@@ -48,8 +53,8 @@ db %>% str()
 # on recode toutes les variables caractères en facteurs
 # on centre-réduit les prédicteurs numériques
 
-db <- db %>% 
-  mutate(y=ifelse(y=="no",0,1))
+#db <- db %>% 
+#  mutate(y=ifelse(y=="no",0,1))
 
 # ICI, ATTENTION SI UN FACTEUR EST CODE EN NUMERIQUE ORDONNE
 my_recipe = recipe(Y ~ ., data = db) %>% 
@@ -60,9 +65,9 @@ don <- my_recipe %>%
   prep() %>% 
   bake(new_data = db)
 
-don <- db %>% 
-  rename(Y=y) %>% 
-    mutate(Y=as.factor(Y))
+#don <- db %>% 
+#  rename(Y=y) %>% 
+#    mutate(Y=as.factor(Y))
 
 # Comparaison d'algo ----
 # Division en Apprentissage et Test
@@ -78,7 +83,7 @@ donTest = don[-A,]
 
 # Division en Apprentissage et Validation
 
-nbloc=5
+nbloc=10
 
 folds = createFolds(donApp$Y,
                     k=nbloc,
@@ -108,7 +113,7 @@ ctrl = trainControl(method='cv', number=3)
 
 
 # Validation croisee
-SCORE=data.frame('Y'=donApp$Y,'logistic'=NA,'aic'=NA,'bic'=NA,'ridge'=NA,'lasso'=NA,'elnet'=NA,'foret'=NA,'rad_svm'=NA,'pol_svm'=NA,'xgb'=NA)
+SCORE=data.frame('Y'=donApp$Y,'logistic'=NA,'aic'=NA,'bic'=NA,'ridge'=NA,'lasso'=NA,'elnet'=NA,'foret'=NA,'rad_svm'=NA,'pol_svm'=NA,'gbm'=NA,'xgb'=NA)
 SCORE %>% head()
 SCORE %>% dim()
 
@@ -191,21 +196,29 @@ for(jj in 1:nbloc){
   
   
   # SVM ========================================================================
-  svm.poly = train(Y~., data=donA, method = 'svmPoly', trControl = ctrl, tuneGrid = gr.poly)
-  bestPoly=svm.poly$results[which.max(svm.poly$results$Accuracy),]
-  tmpPol=ksvm(Y~., data=donA, kernel = 'polydot', kpar=list(degree=bestPoly$degree,scale=1,offset=1),C=bestPoly$C,prob.model=T)
+  #svm.poly = train(Y~., data=donA, method = 'svmPoly', trControl = ctrl, tuneGrid = gr.poly)
+  #bestPoly=svm.poly$results[which.max(svm.poly$results$Accuracy),]
+  #tmpPol=ksvm(Y~., data=donA, kernel = 'polydot', kpar=list(degree=bestPoly$degree,scale=1,offset=1),C=bestPoly$C,prob.model=T)
+  #
+  #svm.radial = train(Y~., data=donA, method = 'svmRadial', trControl = ctrl, tuneGrid = gr.radial)
+  #bestRadial=svm.radial$results[which.max(svm.radial$results$Accuracy),]
+  #tmpRad=ksvm(Y~., data=donA, kernel =  'rbfdot', kpar=list(sigma=bestRadial$sigma),C=bestRadial$C,prob.model=T)
+  #
+  #SCORE[folds==jj,'pol_svm'] = predict(tmpPol, newdata=donV, type = 'prob')[,'1'] # rajouter l'index de colonne : il ne faut en prendre qu'une
+  #SCORE[folds==jj,'rad_svm'] = predict(tmpRad, newdata=donV, type= 'prob')[,'1']  
   
-  svm.radial = train(Y~., data=donA, method = 'svmRadial', trControl = ctrl, tuneGrid = gr.radial)
-  bestRadial=svm.radial$results[which.max(svm.radial$results$Accuracy),]
-  tmpRad=ksvm(Y~., data=donA, kernel =  'rbfdot', kpar=list(sigma=bestRadial$sigma),C=bestRadial$C,prob.model=T)
-  
-  SCORE[folds==jj,'pol_svm'] = predict(tmpPol, newdata=donV, type = 'prob')[,'1'] # rajouter l'index de colonne : il ne faut en prendre qu'une
-  SCORE[folds==jj,'rad_svm'] = predict(tmpRad, newdata=donV, type= 'prob')[,'1']  
-  
-
  
-  # GBM  
-
+  ## =============================================================================
+  ## Gradient Boosting
+  ## =============================================================================
+  # 1. Prepare data: ensure binary response is 0/1
+  donA.gbm = donA %>% mutate(Y = as.numeric(Y) - 1)
+  
+  tmp <- gbm(Y~.,data=donA.gbm,distribution = "bernoulli",cv.folds=5,n.trees=15,
+             interaction.depth = 3,shrinkage = 0.1)
+  best.iter <- gbm.perf(tmp, method = "cv")
+  SCORE[folds==jj,"gbm"] = predict.gbm(tmp,donV,type='response',n.trees = best.iter)
+  
   # XGBOOST =====================================================================
   
   indY = which(names(donA)=='Y')
@@ -237,7 +250,7 @@ for(jj in 1:nbloc){
   #> 
   for(pp in 1:nrow(xgrid)){
     
-    cat(pp/nrow(xgrid),'\n')
+    cat('\n',100*pp/nrow(xgrid),'% \n')
     
     params = list(
       objective = "binary:logistic",
@@ -246,7 +259,7 @@ for(jj in 1:nbloc){
       eta = xgrid$eta[pp]
     )
     
-    nIter = 400
+    nIter = 600
     
     tmp = xgb.cv(
       params = params,
@@ -259,22 +272,28 @@ for(jj in 1:nbloc){
       
     bestIter = tmp$best_iteration
     
-    if(bestIter==nIter){
-      cat('\n',"ATTENTION: BORD DE GRILLE: ", bestIter, '\n')
-    } else{
-        cat('\n' ,'Meilleure itération XGBoost : ', bestIter, '\n' )
-    }
+    #if(bestIter==nIter){
+    #  cat('\n',"ATTENTION: BORD DE GRILLE: ", bestIter, '\n')
+    #} else{
+    #    cat('\n' ,'Meilleure itération XGBoost : ', bestIter, '\n' )
+    #}
     
     bestLogloss = tmp$evaluation_log$test_logloss_mean[bestIter]
     
     x = data.frame(cbind(xgrid[pp,],bestIter,bestLogloss))
     
     results[pp,1:nrow(xgrid)] = x
-    
-
   }
   
   best_params_index <- results[which.max(results$bestLogloss),] 
+  
+  checkIter = best_params_index$bestIter
+  
+  if(checkIter == nIter){
+      cat('\n', "ATTENTION: BORD DE GRILLE --> ", checkIter)
+  }else{
+      cat('\n','Meilleure itération XGBoost: ', checkIter)
+  }
   
   best_params = list(
     objective = "binary:logistic",
@@ -283,7 +302,7 @@ for(jj in 1:nbloc){
     eta = best_params_index$eta
   )
   
-  xgb = xgb.train(params = best_params,data = xgb_data_train,nrounds = best_params_index$bestIter)
+  xgb = xgb.train(params = best_params,data = xgb_data_train, nrounds = best_params_index$bestIter)
   
   SCORE[folds==jj, 'xgb']=  predict(xgb,newdata = xgb_data_test,type='response')
   
@@ -291,21 +310,32 @@ for(jj in 1:nbloc){
 
 SCORE
 
-rocCV = roc(factor(Y)~., data=SCORE, quiet=T)
+rocCV = roc(factor(Y)~., data=SCORE %>% select(-c(rad_svm,pol_svm)), quiet=T)
 
 aucmodele = sort(round(unlist(lapply(rocCV,auc)),5),dec=TRUE)
 
-tmp = lapply(rocCV, FUN=coords, x = 'best', 
-             ret = c('threshold','tp','fp','fn','tn','sensitivity','specificity','ac'),
-             transpose=T)
-
+tmp = lapply(rocCV, FUN = function(r) {
+    co = coords(r, x = "best",
+                ret = c('threshold','tp','fp','fn','tn','sensitivity','specificity','accuracy'),
+                transpose = TRUE)
+    
+    # Compute F1 score
+    tp = co["tp"]
+    fp = co["fp"]
+    fn = co["fn"]
+    f1 = if ((2 * tp + fp + fn) == 0) NA else 2 * tp / (2 * tp + fp + fn)
+    
+    # Add F1 to the coords result
+    co["f1"] <- f1
+    return(co)
+})
 mat=do.call(rbind,tmp)
 
 aucmodele
 mat
 
-# REESTIMER LE MODELE SUR l4ENSEMBLE DES DONNES ET PREDIRE LE TEST
-# faire le feature engenering
+# REESTIMER LE MODELE SUR l'ENSEMBLE DES DONNES ET PREDIRE LE TEST
+# faire le feature engineering
 
 # Meilleur modèle => xgboost donc on le récupère:
 
@@ -333,18 +363,78 @@ SCORE_final
 
 rocCV = roc(factor(Y)~., data=SCORE_final, quiet=T)
 
-aucmodele = sort(round(unlist(lapply(rocCV,auc)),5),dec=TRUE)
-lapply(rocCV,auc)
+auc(rocCV)
 
 
-tmp = lapply(rocCV, FUN=coords, x = 'best', 
-             ret = c('threshold','tp','fp','fn','tn','sensitivity','specificity','ac'),
-             transpose=T)
+#===============================================================================
+# > Feature engineering
+#=============================================================================== 
 
-mat=do.call(rbind,tmp)
+### 1. Polynomes
 
-aucmodele
-mat
+carre = function(x){
+    return(x^2)
+}
+cube = function(x){
+    return(x^3)
+}
+
+index.num.app = sapply(donApp, is.numeric)
+index.num.app
+
+donApp.squared = sapply(donApp[,index.num.app],carre)
+colnames(donApp.squared) = paste0('sqd_',colnames(donApp.squared))
+donApp.cubed   = sapply(donApp[,index.num.app],cube)
+colnames(donApp.cubed) = paste0('cub_',colnames(donApp.cubed))
+
+donTest.squared = sapply(donTest[,index.num.app], carre)
+colnames(donTest.squared) = paste0('sqd_',colnames(donTest.squared))
+donTest.cubed = sapply(donTest[,index.num.app],cube)
+colnames(donTest.cubed) = paste0('cub_',colnames(donTest.cubed))
+
+
+donApp.poly = data.frame(cbind(donApp,donApp.squared, donApp.cubed))
+donTest.poly = data.frame(cbind(donTest,donTest.squared, donTest.cubed))
+
+mat.donApp.poly = model.matrix(Y~., data= donApp.poly) 
+donApp.Y = as.numeric(donApp$Y) -1
+
+mat.donTest.poly = model.matrix(Y~., data=donTest.poly)
+
+xgb.data.poly = xgb.DMatrix(data=mat.donApp.poly, label = donApp.Y)
+
+xgb.test.poly = xgb.DMatrix(data=mat.donTest.poly)
+donTest.Y = as.numeric(donTest$Y)-1
+
+# 
+SCORE.poly = data.frame('Y' = donTest.Y)
+#
+
+nIter = 10
+
+params = list(
+    objective = "binary:logistic",
+    eval_metric = "logloss",
+    max_depth = 3,
+    eta = 0.1
+)
+
+model.poly <- xgb.train(
+    params = params,
+    data = xgb.data.poly,
+    nrounds = nIter,
+    verbose = 0
+)
+
+SCORE.poly[,'predict_poly']=predict(model.poly, newdata = xgb.test.poly, type='response')
+
+rocCV = roc(factor(Y)~., data=SCORE.poly, quiet=T)
+
+auc(rocCV)
+
+
+
+
 
 
 
